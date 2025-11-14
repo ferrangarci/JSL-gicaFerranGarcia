34f05860a1510e801af697799828ac4cd72ed6fd
Ferran <ferran.garcia@institutvidreres.cat>
2025-11-14 11:53:34 +0100

    Ex2

--- javascript1.js
+++ javascript1.js
@@ -1,7 +1,16 @@
 // EXERCICI 1
-let x = 4;
+/* let x = 4;
 if (x % 3 == 0 || x % 7 == 0) {
     console.log("És múltiple de 3 o de 7");
 } else {
     console.log("No és múltiple");
 }
+ */
+
+// EXERCICI 2
+let x = 15
+let y = 99
+let r1 = (x >= 10 && x <= 50) ? "x està en rang" : "x fora de rang";
+let r2 = (y >= 10 && y <= 50) ? "y està en rang" : "y fora de rang";
+console.log(r1);
+console.log(r2);
