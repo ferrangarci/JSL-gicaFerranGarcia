9c97dd777769ec392ff909c2ef81d065e3c8e779
Ferran <ferran.garcia@institutvidreres.cat>
2025-11-14 12:04:15 +0100

    Ex3

--- javascript1.js
+++ javascript1.js
@@ -8,9 +8,19 @@ if (x % 3 == 0 || x % 7 == 0) {
  */
 
 // EXERCICI 2
-let x = 15
+/* let x = 15
 let y = 99
 let r1 = (x >= 10 && x <= 50) ? "x està en rang" : "x fora de rang";
 let r2 = (y >= 10 && y <= 50) ? "y està en rang" : "y fora de rang";
 console.log(r1);
 console.log(r2);
+ */
+
+// EXERCICI 3
+let x = 6;
+
+if (x < 5) console.log("Suspès");
+else if (x < 6) console.log("Suficient");
+else if (x < 7) console.log("Bé");
+else if (x < 9) console.log("Notable");
+else console.log("Excel·lent");
